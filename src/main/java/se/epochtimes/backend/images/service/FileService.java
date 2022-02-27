fb14cb257cba283104d92b063de050565f8f6662
Marten Hernebring <marten.hernebring@gmail.com>
2022-02-27 17:17:28 +0100

    Added code from video storing file impl

--- src/main/java/se/epochtimes/backend/images/service/FileService.java
+++ src/main/java/se/epochtimes/backend/images/service/FileService.java
@@ -1,5 +1,8 @@
 package se.epochtimes.backend.images.service;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
@@ -8,9 +11,27 @@ import java.util.Optional;
 
 @Service
 public class FileService {
+
+  private final AmazonS3 s3;
+
+  public FileService(AmazonS3 s3) {
+    this.s3 = s3;
+  }
+
   public void save(String path,
                    String fileName,
                    InputStream inputStream,
                    Optional<Map<String, String>> optionalMetaData) {
+    ObjectMetadata metadata = new ObjectMetadata();
+    optionalMetaData.ifPresent(map -> {
+      if(!map.isEmpty()) {
+        map.forEach(metadata::addUserMetadata);
+      }
+    });
+    try{
+      s3.putObject(path, fileName, inputStream, metadata);
+    } catch(AmazonServiceException e) {
+      throw new IllegalStateException("Failed to store file to s3", e);
+    }
   }
 }
